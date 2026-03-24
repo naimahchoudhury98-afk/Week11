636f29f1822125408f82f7ff8f46a89bd4f2b8e8
Sarah <sarahrooney130500@gmail.com>
2026-03-24 13:55:51 +0000

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,19 +1,10 @@
 export default function Footer() {
   return (
-    <footer style={styles.footer}>
-      <p>
-        © {new Date().getFullYear()} 2026 Meal Planner & Recipes Website Created By
-        Mahirah, Marisa, Naimah and Sarah{" "}
+    <footer role="contentinfo" aria-label="Site footer">
+      <p aria-live="polite">
+        © {new Date().getFullYear()} Meal Planner & Recipes Website Created By
+        Mahirah, Marisa, Naimah and Sarah
       </p>
     </footer>
   );
 }
-
-const styles = {
-  footer: {
-    textAlign: "center",
-    padding: "20px",
-    borderTop: "1px solid #eaeaea",
-    marginTop: "40px",
-  },
-};
